ec44e8dc19d8c0fd0f2f2c59937ddd42cb0e64d1
Uyen Tran <tranphuonguyen1011@gmail.com>
2026-08-22 21:45:49 +0100

    integrate MovieInfo and Review web clients for MovieService

--- movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
+++ movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
@@ -1,5 +1,7 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.client.MoviesInfoRestClient;
+import com.reactivespring.client.ReviewRestClient;
 import com.reactivespring.domain.Movie;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,25 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/v1/movies")
 public class MoviesController {
 
+    private MoviesInfoRestClient moviesInfoRestClient;
+    private ReviewRestClient reviewRestClient;
+
+    public MoviesController(MoviesInfoRestClient moviesInfoRestClient, ReviewRestClient reviewRestClient) {
+        this.moviesInfoRestClient = moviesInfoRestClient;
+        this.reviewRestClient = reviewRestClient;
+    }
+
     @GetMapping("/{id}")
     public Mono<Movie> retrieveMovieById(@PathVariable("id") String movieId) {
-        return null;
+
+        return moviesInfoRestClient
+                .retrieveMovieInfo(movieId)
+                .flatMap(movieInfo -> {
+                    var reviewsListMono = reviewRestClient.retrieveReviews(movieId)
+                            .collectList();
+
+                    return reviewsListMono.map(reviews -> new Movie(movieInfo, reviews));
+                });
     }
 
 }
